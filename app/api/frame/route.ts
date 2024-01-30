66245d019a5500547f5c82d82b879edf175ad4a5
Amit Shah <amit.shah@coinbase.com>
2024-01-29 20:57:49 -0500

    add token image

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -45,13 +45,16 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   //sdk.updateSignerOrProvider(w);
   let sdk = ThirdwebSDK.fromSigner(w, 'base', { secretKey: process.env.THIRDWEB_SECRET_KEY });
   let contract = await sdk.getContract('0x6F45df69821667E38CBc5A249ABa11df12c73645');
+  let md = await contract.erc1155.getTokenMetadata(0);
   let tx = await contract.erc1155.claimTo.prepare(accountAddress as string, 0, 1);
+
   const cc = await contract.erc1155.claimConditions.prepareClaim(
     0,
     1,
     false,
     accountAddress as string,
   );
+
   const priceWei = cc.price;
   const priceEther = ethers.utils.formatEther(priceWei);
   console.log('price:', cc.price);
@@ -60,12 +63,14 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   if (accountAddress === undefined) {
     return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
+    <meta property="og:image" content=${md.image}/>
     <meta property="fc:frame:button:1" content="Need Address To Mint" />
   </head></html>`);
   }
   let random = Math.random().toString();
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
+    <meta property="og:image" content={${md.image}}/>
     <meta property="fc:frame:button:1" content="Cannot Mint - Try Viewing On Wallet" />
     <meta property="fc:frame:post_url" content="https://test-frame-mint.vercel.app/api/frame" />
     <meta property="cb:tx" content="to:0x6F45df69821667E38CBc5A249ABa11df12c73645,data:${encoded},value:${priceEther}" />
